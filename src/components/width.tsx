e1cef5c4c861c168ce85af7e3a01697fef135b10
Bruno Costa <br9uno@gmail.com>
2024-04-02 17:11:31 -0300

    [fix] refatoração de código

--- src/components/width.tsx
+++ src/components/width.tsx
@@ -3,18 +3,15 @@
 import React, { useEffect, useState } from "react";
 
 export default function Width() {
-  const [width, setWidth] = useState(0);
+  const [width, setWidth] = useState(
+    () => document.documentElement.clientWidth
+  );
   const [ativo, setAtivo] = useState(false);
 
   useEffect(() => {
-    const handleResize = () => {
-      setWidth(document.documentElement.clientWidth);
-    };
-    handleResize();
+    const handleResize = () => setWidth(document.documentElement.clientWidth);
     window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
@@ -22,7 +19,7 @@ export default function Width() {
       <h2 style={{ color: ativo ? "#680" : "#b00" }}>
         Largura da tela: {width}
       </h2>
-      <button onClick={() => setAtivo((s) => !s)}>Ativar</button>
+      <button onClick={() => setAtivo((ativo) => !ativo)}>Ativar</button>
     </div>
   );
 }
